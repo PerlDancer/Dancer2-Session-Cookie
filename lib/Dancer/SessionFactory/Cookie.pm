1feeff6df630f6f5114270ca7455221ad7a59579
David Golden <dagolden@cpan.org>
2013-01-09 22:40:39 -0500

    added documentation

--- lib/Dancer/SessionFactory/Cookie.pm
+++ lib/Dancer/SessionFactory/Cookie.pm
@@ -26,8 +26,6 @@ with 'Dancer::Core::Role::SessionFactory';
 
 =attr secret_key
 
-  secret_key: your secret pass phrase here
-
 This is used to secure the cookies.  Encryption keys and message authentication
 keys are derived from this using one-way functions.  Changing it will
 invalidate all sessions.
@@ -187,20 +185,114 @@ sub _sessions { return [] }
 
 =head1 SYNOPSIS
 
-  use Dancer::SessionFactory::Cookie;
+  # In Dancer 2 config.yml file
+
+  session: Cookie
+  engines:
+    session:
+      Cookie:
+        secret_key: your secret passphrase
+        max_duration: 604800
 
 =head1 DESCRIPTION
 
-This module might be cool, but you'd never know it from the lack
-of documentation.
+This module implements a session factory for Dancer 2 that stores session
+state within a browser cookie.  It uses AES encryption to protect
+session data, an expiration timestamp (within the cookie value itself) to
+enforce session expiration, and a MAC to ensure integrity.
 
-=head1 USAGE
+=head1 LIMITATIONS AND SECURITY
 
-Good luck!
+=head2 Session size
+
+Cookies must fit within 4k, so don't store too much data in the session.
+This module uses L<Sereal> for serialization and does enable the C<snappy>
+compression option, which kicks in over 1K.
+
+=head2 Secret key
+
+You must protect the secret key, of course.  Rekeying periodically would
+improve security.  Rekeying also invalidates all existing sessions.  In a
+multi-node application, all nodes must share the same secret key.
+
+=head2 Transport security
+
+While session data is encrypted, an attacker could intercept cookies and replay
+them to impersonate a valid user.  SSL encryption is strongly recommended.
+
+=head2 Cookie replay
+
+Because all session state is maintained in the session cookie, an attacker
+or malicious user could replay an old cookie to return to a previous state.
+Cookie-based sessions should not be used for recording incremental steps
+in a transaction or to record "negative rights".
+
+Because cookie expiration happens on the client-side, an attacker or malicious
+user could replay a cookie after its scheduled expiration date.  It is strongly
+recommended to set C<cookie_duration> or C<max_duration> to limit the window of
+opportunity for such replay attacks.
+
+=head2 Session authentication
+
+A compromised secret key could be used to construct valid messages appearing to
+be from any user.  Applications should take extra steps in their use of session
+data to ensure that sessions are authenticated to the user.
+
+One simple approach could be to store a hash of the user's hashed password
+in the session on login and to verify it on each request.
+
+  # on login
+  my $hashed_pw = bcrypt( $password );
+  if ( $hashed_pw eq $hashed_pw_from_db ) {
+    session user => $user;
+    session auth => bcrypt( $hashed_pw ) );
+  }
+
+  # on each request
+  if ( bcrypt( $hashed_pw_from_db ) ne session("auth") ) {
+    context->destroy_session;
+  }
+
+The downside of this is that if there is a read-only attack against the
+database (SQL injection or leaked backup dump) and the secret key is compromised,
+then an attacker can forge a cookie to impersonate any user.
+
+A more secure approach suggested by Stephen Murdoch in "Hardened Stateless
+Session Cookies", is to store an iterated hash of the hashed password in the
+database and use the hashed password itself within the session.
+
+  # on login
+  my $hashed_pw = bcrypt( $password );
+  if ( bcrypt( $hashed_pw ) eq $double_hashed_pw_from_db ) {
+    session user => $user;
+    session auth => $hashed_pw;
+  }
+
+  # on each request
+  if ( $double_hashed_pw_from_db ne bcrypt( session("auth") ) ) {
+    context->destroy_session;
+  }
+
+This latter approach means that even a compromise of the secret key and the
+database contents can't be used to impersonate a user because doing so would
+requiring reversing a one-way hash to determine the correct authenticator to
+put into the forged cookie.
 
 =head1 SEE ALSO
 
-Maybe other modules do related things.
+Papers on secure cookies and cookie session storage:
+
+=for :list
+* Liu, Alex X., et al., L<A Secure Cookie Protocol|http://www.cse.msu.edu/~alexliu/publications/Cookie/Cookie_COMNET.pdf>
+* Murdoch, Stephen J., L<Hardened Stateless Session Cookies|http://www.cl.cam.ac.uk/~sjm217/papers/protocols08cookies.pdf>
+* Fu, Kevin, et al., L<Dos and Don'ts of Client Authentication on the Web|http://pdos.csail.mit.edu/papers/webauth:sec10.pdf>
+
+Other CPAN modules providing cookie session storage (possibly for other frameworks):
+
+=for :list
+* L<Dancer::Session::Cookie> -- Dancer 1 precursor to this module, encryption only, no MAC
+* L<Plack::Middleware::Session::Cookie> -- MAC only
+* L<Catalyst::Plugin::CookiedSession> -- encryption only
 
 =cut
 
